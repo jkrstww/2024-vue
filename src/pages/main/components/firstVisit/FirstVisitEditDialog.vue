0fe6a2d6dd8a45cd10a302a8127f9ff0abbcdf08
WusenSwain <1208532105@qq.com>
2024-06-27 11:55:50 +0800

    初访员页面

---
+++ src/pages/main/components/firstVisit/FirstVisitEditDialog.vue
@@ -0,0 +1,67 @@
+<!--初访结论编辑按钮-->
+<template>
+  <div>
+    <el-card class="container">
+      <el-dialog
+          title="初访结论"
+          :visible.sync="dialogVisible"
+          width="30%">
+        <el-form ref="form" label-width="80px">
+          <el-form-item label="危机等级">
+            <el-select v-model="form.level" placeholder="请选择危机等级">
+              <el-option label="安全" :value="1"></el-option>
+              <el-option label="中等" :value="2"></el-option>
+              <el-option label="危险" :value="3"></el-option>
+            </el-select>
+          </el-form-item>
+          <el-form-item label="问题类型">
+            <el-select v-model="form.type" placeholder="请选择问题类型">
+              <el-option label="抑郁" :value="1"></el-option>
+              <el-option label="焦虑" :value="2"></el-option>
+              <el-option label="双向情感障碍" :value="3"></el-option>
+              <el-option label="躁狂" :value="4"></el-option>
+            </el-select>
+          </el-form-item>
+          <el-form-item label="初访结论">
+            <el-select v-model="form.conclusion" placeholder="请选择初访结论">
+              <el-option label="无需咨询" :value="1"></el-option>
+              <el-option label="安排咨询" :value="2"></el-option>
+              <el-option label="转介送诊" :value="3"></el-option>
+            </el-select>
+          </el-form-item>
+        </el-form>
+        <span slot="footer" class="dialog-footer">
+          <el-button @click="dialogVisible = false">取 消</el-button>
+          <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+        </span>
+      </el-dialog>
+    </el-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FirstVisitEditDialog',
+  data () {
+    return {
+      dialogVisible: false,
+      form: {
+        level: '',
+        type: '',
+        date: '',
+        conclusion: ''
+      }
+    }
+  },
+  methods: {
+    show (students) {
+      this.dialogVisible = true
+      this.form = JSON.parse(JSON.stringify(students))
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/pages/main/routes/firstVisitor/Conclusion.vue
+++ src/pages/main/routes/firstVisitor/Conclusion.vue
@@ -10,20 +10,175 @@
       </div>
     </el-card>
     <el-card class="container">
-      {{title}}
+      <el-form :inline="true" class="demo-form-inline" style="margin-top: 20px;margin-left: 20px">
+        <el-form-item label="学号">
+          <el-input v-model="sn" placeholder="学号"></el-input>
+        </el-form-item>
+        <el-form-item label="姓名">
+          <el-input v-model="name" placeholder="姓名"></el-input>
+        </el-form-item>
+        <el-form-item label="校区">
+          <el-select v-model="campusVal" placeholder="校区">
+            <el-option label="全部" value=""></el-option>
+            <el-option label="望江校区" value="1"></el-option>
+            <el-option label="江安校区" value="2"></el-option>
+            <el-option label="华西校区" value="3"></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="学院">
+          <el-select v-model="collegeVal" placeholder="学院">
+            <el-option label="全部" value=""></el-option>
+            <el-option label="计算机学院" value="1"></el-option>
+            <el-option label="艺术学院" value="2"></el-option>
+            <el-option label="数学学院" value="3"></el-option>
+            <el-option label="外国语学院" value="4"></el-option>
+            <el-option label="经济学院" value="5"></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click = "getStudents" icon="el-icon-search">查询</el-button>
+        </el-form-item>
+      </el-form>
+      <el-table
+          :data="students"
+          style="width: 100%"
+          height="700">
+        <el-table-column
+            prop="sn"
+            label="学号"
+            width="180">
+        </el-table-column>
+        <el-table-column
+            prop="name"
+            label="姓名"
+            width="180">
+        </el-table-column>
+        <el-table-column
+            prop="gender"
+            label="性别">
+          <template slot-scope="scope">
+            {{ gender[scope.row.gender]}}
+          </template>
+        </el-table-column>
+        <el-table-column
+            prop="campus"
+            label="校区">
+          <template slot-scope="scope">
+            {{ campus[scope.row.campus]}}
+          </template>
+        </el-table-column>
+        <el-table-column
+            prop="college"
+            label="学院">
+          <template slot-scope="scope">
+            {{ college[scope.row.college]}}
+          </template>
+        </el-table-column>
+        <el-table-column
+            prop="phoneNumber"
+            label="联系方式">
+        </el-table-column>
+        <el-table-column
+            prop="appointmentDate"
+            label="预约时间">
+        </el-table-column>
+        <el-table-column
+            fixed="right"
+            label="操作"
+            width="100">
+          <template slot-scope="scope">
+            <el-button type="text" size="small" @click="visitEdit(scope.row)">初访结论</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+      <FirstVisitEditDialog @ok="getStudents" ref="visitEdit"></FirstVisitEditDialog>
+      <el-pagination
+          background
+          @size-change="handleSizeChange"
+          @current-change="handleCurrentChange"
+          :current-page="pageNo"
+          :page-sizes="[10, 20, 40, 100]"
+          :page-size="pageSize"
+          layout="total, sizes, prev, pager, next, jumper"
+          :total="totals">
+      </el-pagination>
     </el-card>
   </div>
 </template>
 
 <script>
+import {pageVisitList} from '@api/api'
+import FirstVisitEditDialog from '@/pages/main/components/firstVisit/FirstVisitEditDialog.vue'
 export default {
-  name: 'statistic',
+  name: 'conclusion',
   data () {
     return {
-      title: '初访结论'
+      students: [],
+      sn: '',
+      name: '',
+      campusVal: '',
+      collegeVal: '',
+      totals: 0,
+      pageNo: 1,
+      pageSize: 10
+    }
+  },
+  components: {
+    FirstVisitEditDialog
+  },
+  computed: {
+    gender () {
+      return {
+        1: '男',
+        0: '女'
+      }
+    },
+    campus () {
+      return {
+        1: '望江校区',
+        2: '江安校区',
+        3: '华西校区'
+      }
+    },
+    college () {
+      return {
+        1: '计算机学院',
+        2: '艺术学院',
+        3: '数学学院',
+        4: '外国语学院',
+        5: '经济学院'
+      }
     }
   },
   methods: {
+    getStudents () {
+      let obj = {
+        pageNo: this.pageNo,
+        pageSize: this.pageSize,
+        sn: this.sn,
+        name: this.name,
+        campus: this.campusVal,
+        college: this.collegeVal
+      }
+      pageVisitList(obj).then(res => {
+        this.students = res.data.records
+        this.totals = res.data.total
+      })
+    },
+    handleSizeChange (val) {
+      this.pageSize = val
+      this.getStudents()
+    },
+    handleCurrentChange (val) {
+      this.pageNo = val
+      this.getStudents()
+    },
+    visitEdit (student) {
+      this.$refs.visitEdit.show(student)
+    }
+  },
+  created () {
+    this.getStudents()
   }
 }
 </script>
